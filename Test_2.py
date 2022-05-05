ffbe1c0dde1b3ebc6f5739c3a6a9631a235906e1
yueyuelaojun680319 <usicdiet@126.com>
2022-05-05 14:36:01 +0800

    Second Python File

--- Test_2.py
+++ Test_2.py
@@ -1,2 +1,8 @@
-for _ in range(10):
-    print('hi')
+country = input('请问你是那个国家？')
+age = input('请输入您的年龄：')
+age = int(age)
+if country == '台湾':
+    if age >= 18:
+        print('您可以考驾照')
+    else:
+        print('你还不能考驾照！')
