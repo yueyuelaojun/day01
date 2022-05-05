cb5156386d89d239754148bb81c6be5b07eb6808
yueyuelaojun680319 <usicdiet@126.com>
2022-05-05 23:00:48 +0800

    Youhua last time no

--- Test_3.py
+++ Test_3.py
@@ -1,9 +1,14 @@
+pwd = 'a123456'
 i = 3
-while True:
+while i > 0:
     password = input('请输入密码：')
-    if password == 'a123456':
+    if password == pwd:
         print('登录成功！')
         break
     else:
         i = i - 1
-        print('密码错误，还有', i, '次机会')
+        if i>0:
+            print('密码错误，还有', i, '次机会')
+        else:
+            print('密码错误!')
+
