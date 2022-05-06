7ca98f38ff7149b479a6e7f9851816014c44a9e0
yueyuelaojun680319 <usicdiet@126.com>
2022-05-06 23:39:02 +0800

    Test_6 File

---
+++ Test_6.py
@@ -0,0 +1,13 @@
+cars = ['Toyota', 'Honda', 'Audi']
+for car in cars:
+    print(car)
+names = ['Allen', 'Tom', 'Jack']
+for name in names:
+    print(name)
+
+car='Audi'
+for c in car:
+    print(c)
+print(len(car))
+print('A' in car)
+print('x' in car)
