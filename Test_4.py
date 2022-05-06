7df465b6b402c502f22d53da40c25077be1ab560
yueyuelaojun680319 <usicdiet@126.com>
2022-05-06 13:10:31 +0800

    Youhuale counts

--- Test_4.py
+++ Test_4.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     num = input('请猜数字：')
     num = int(num)
-    if num==r:
+    if num == r:
         print('猜中了！')
+        print('这是你猜的第', count, '次')
         break
-    elif num>r:
+    elif num > r:
         print('大了！')
-    elif num<r:
+    elif num < r:
         print('小了！')
+    print('这是你猜的第', count, '次')
